66153dd51e70f185f3c97e0ae25a9b5febf10d39
Silvio Huan Matheus Grein Cristo Ribeiro <silviohuan@gmail.com>
2025-06-30 20:32:53 -0400

    Um commit ai

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import './style.css'
+
+const menu = document.querySelector<HTMLDivElement>('.menu')!
+const burguer = document.querySelector<HTMLImageElement>('.burguer')!
+
+burguer.addEventListener('click', ()=>{
+    burguer.classList.toggle('active')
+    burguer.src = burguer.src === "http://localhost:5173/public/menu.svg" ? "http://localhost:5173/public/circle-x.svg":"/public/menu.svg"
+    menu.classList.toggle('active')
+})
